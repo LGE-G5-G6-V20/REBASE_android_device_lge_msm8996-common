e67f0750447b8a5969465058f42708c03119348d
xxseva44 <dev.xxseva44@gmail.com>
2022-06-28 22:21:37 -0400

    Add support for soundtrigger V2.1

--- msm8996.mk
+++ msm8996.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
