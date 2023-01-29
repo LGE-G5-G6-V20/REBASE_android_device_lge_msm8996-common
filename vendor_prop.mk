b623382a41b558973081863d06b385165353cbe0
xxseva44 <dev.xxseva44@gmail.com>
2023-01-29 15:51:32 -0500

    Drop netflix tweak

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -160,11 +160,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable.pq=true \
     vendor.video.disable.ubwc=1
-
-# Netflix
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.netflix.bsp_rev=Q660-13149-1
-
+    
 # Bye google
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.nocheckin=1
