9e160bf7695c3beca81aa007085e8ed5822ecf6f
xxseva44 <dev.xxseva44@gmail.com>
2023-01-29 02:28:18 -0500

    Get rid of perf profile

--- msm8996.mk
+++ msm8996.mk
@@ -325,8 +325,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power.stats@1.0-service.mock 
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QMI
 PRODUCT_PACKAGES += \
