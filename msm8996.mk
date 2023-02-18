f3605388816e9cf4f77d94fd5e50a9eff6d78556
xxseva44 <dev.xxseva44@gmail.com>
2023-02-17 22:27:42 -0500

    Thermal: Add missing dependencies

--- msm8996.mk
+++ msm8996.mk
@@ -379,6 +379,12 @@ PRODUCT_PACKAGES += \
     libcutils_shim
     
 # Pixel thermal
+SOONG_CONFIG_NAMESPACES += thermal_hal_feature
+SOONG_CONFIG_thermal_hal_feature += \
+    pid \
+
+SOONG_CONFIG_thermal_hal_feature_pid := apply_1_0
+
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel \
     thermal_symlinks
