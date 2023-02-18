438def0728a6ed1b9028c9f138e0ac083c34e188
xxseva44 <dev.xxseva44@gmail.com>
2023-02-18 14:11:48 -0500

    Fix logging rc file include

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -82,11 +82,13 @@ LOCAL_VENDOR_MODULE    := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.logging.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.logging.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
