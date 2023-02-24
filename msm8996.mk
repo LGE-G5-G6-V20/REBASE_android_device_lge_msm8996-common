15fab48077698669f8932ea399da76309f7f166b
xxseva44 <dev.xxseva44@gmail.com>
2023-02-23 20:45:53 -0500

    Use google source powerhal rather than local

--- msm8996.mk
+++ msm8996.mk
@@ -320,7 +320,7 @@ PRODUCT_PACKAGES += \
     
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.msm8996-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
